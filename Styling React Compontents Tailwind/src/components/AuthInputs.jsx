f024210d0b15625a40742d1dfd9d27a93b5912d8
MarioTorresRonda <mariotorresrma@gmail.com>
2023-12-04 22:32:05 +0100

    Subida General 7
    https://www.udemy.com/course/react-the-complete-guide-incl-redux - 140

--- Styling React Compontents Tailwind/src/components/AuthInputs.jsx
+++ Styling React Compontents Tailwind/src/components/AuthInputs.jsx
@@ -1,14 +1,6 @@
 import { useState } from 'react';
-import{ styled } from 'styled-components'
 import Button from './Button'
 import LabelInput from './LabelInput';
-
-const ControlDiv = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -31,8 +23,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlDiv>
+    <div id="auth-inputs" className='w-full max-w-sm mx-auto p-8 rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
         <LabelInput label="Email" invalid={emailNotValid} type="email" onChange={(event) => handleInputChange('email', event.target.value)} />
         <LabelInput label="Password" invalid={passwordNotValid} type="password" onChange={(event) => handleInputChange('password', event.target.value) } />
         {/* <p className='paragraph' >
@@ -53,9 +45,9 @@ export default function AuthInputs() {
             }
           />
         </p> */}
-      </ControlDiv>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className='flex justify-end gap-4'>
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button /*className='button'*/ onClick={handleLogin}>Sign In</Button>
