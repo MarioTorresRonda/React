a41bd1f432e83d853db3142488cbb9312bb5f579
MarioTorresRonda <mariotorresrma@gmail.com>
2024-03-01 01:02:16 +0100

    Curso 25 | Pages Router | Vercel - blocking|  Finalizado
    https://www.udemy.com/course/react-the-complete-guide-incl-redux

--- PagesRouterAdvancedNextJS/components/meetups/MeetUpDetail.js
+++ PagesRouterAdvancedNextJS/components/meetups/MeetUpDetail.js
@@ -2,9 +2,9 @@ import classes from './MeetUpDetail.module.css'
 
 export default function MeetUpDetail(props) {
     return <section className={classes.detail}>
-        <img src={props.src} alt=''/>
+        <img src={props.image} alt=''/>
         <h1> {props.title} </h1>
-        <address> {props.adress} </address>
+        <address> {props.address} </address>
         <p> {props.description} </p>
     </section>
 }
--- PagesRouterAdvancedNextJS/pages/new-meetup/index.js
+++ PagesRouterAdvancedNextJS/pages/new-meetup/index.js
@@ -1,9 +1,24 @@
+import { useRouter } from "next/router";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 export default function NewMeetUpPage() {
     
-    function addMeetUpHandler(enteredValues) {
-        console.log( enteredValues );
+    const router = useRouter();
+
+    async function addMeetUpHandler(enteredValues) {
+        const response = await fetch('/api/new-meetup', {
+            method: 'POST',
+            body: JSON.stringify( enteredValues ),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+
+        console.log( data );
+
+        router.push('/');
     }
 
     return ( 
